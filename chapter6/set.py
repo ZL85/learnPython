2334b043b0193c341bfe693c32bee5398ffbfd71
ZL85 <zongliang0726@163.com>
2023-05-29 12:53:37 +0800

    update

--- chapter6/set.py
+++ chapter6/set.py
@@ -61,10 +61,11 @@ print("set3 after clear:", set3)
 # 进行并集运算时使用“｜”符号；
 # 进行差集运算时使用“-”符号；
 # 进行对称差集运算时使用“^”符号。
-python = set(['绮梦', '冷伊一', '香凝', '梓轩'])  # 保存选择Python语言的学生名字
-c = set(['冷伊一', '零语', '梓轩', '圣博'])  # 保存选择C语言的学生名字
-print('选择Python语言的学生有：', python)  # 输出选择Python语言的学生名字
-print('选择C语言的学生有：', c)  # 输出选择C语言的学生名字
-print('交集运算：', python & c)  # 输出既选择了Python语言又选择C语言的学生名字
-print('并集运算：', python | c)  # 输出参与选课的全部学生名字
-print('差集运算：', python - c)  # 输出选择了Python语言但没有选择C语言的学生名字
+set4 = set(['1', '2', '3', '4'])
+set5 = set(['2', '5', '4', '6'])
+print('set4：', set4)
+print('set5：', set5)
+print('the result of "&"：', set4 & set5)
+print('the result of "｜"：', set4 | set5)
+print('the result of "-"：', set4 - set5)
+print('the result of "^":：', set4 ^ set5)
