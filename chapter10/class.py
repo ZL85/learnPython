e5dc99512e4a7b130dfd83e9327098ba89752cc8
ZL85 <zongliang0726@163.com>
2023-06-01 15:08:17 +0800

    update

--- chapter10/class.py
+++ chapter10/class.py
@@ -177,6 +177,9 @@ class Demo8:
 
 
 demo8 = Demo8()
+print()
+
+
 # print(demo8.__member) # 运行报错，无法通过类的实例进行访问
 
 # __name__：首尾双下划线表示定义特殊方法，一般是系统定义名字，如__init__()。
@@ -194,4 +197,135 @@ demo8 = Demo8()
 # self：必要参数，表示类的实例。
 # block：方法体，实现的具体功能。在方法体中，通常以return语句结束，用于返回计算结果。
 
+# 定义一个矩形类，在__init__()方法中定义两个实例属性，
+# 然后再定义一个计算矩形面积的方法，并应用@property将其转换为属性，
+# 最后创建类的实例，并访问转换后的属性
+class Rect:
+    def __init__(self, width, height):
+        self.width = width  # 矩形的宽
+        self.height = height  # 矩形的高
 
+    @property  # 将方法转换为属性
+    def area(self):  # 计算矩形的面积的方法
+        return self.width * self.height  # 返回矩形的面积
+
+
+rect = Rect(800, 600)  # 创建类的实例
+print("面积为:", rect.area)  # 输出属性的值
+print()
+
+
+# 通过@property转换后的属性不能重新赋值，如果对其重新赋值，将抛出异常信息。
+
+# 为属性添加安全保护机制
+# 在Python中，默认情况下，创建的类属性或者实例是可以在类体外进行修改的，
+# 如果想要限制其不能在类体外修改，可以将其设置为私有的，但设置为私有后，在类体外也不能获取它的值。
+# 如果想要创建一个可以读取，但不能修改的属性，那么可以使用@property实现只读属性。
+class Demo9:
+    def __init__(self, output):
+        print("I'm Demo9 class")
+        self.__output = output
+
+    @property
+    def output(self):
+        return self.__output
+
+
+demo9 = Demo9("read only attribute")
+print(demo9.output)
+print()
+
+
+# 继承
+# 在编写类时，并不是每次都要从空白开始。
+# 当要编写的类和另一个已经存在的类之间存在一定的继承关系时，就可以通过继承来达到代码重用的目的，提高开发效率。
+
+# 继承的基本语法
+# 在程序设计中实现继承，表示这个类拥有它继承的类的所有公有成员或者受保护成员。
+# 在面向对象编程中，被继承的类称为父类或基类，新的类称为子类或派生类。
+# 通过继承不仅可以实现代码的重用，还可以通过继承来理顺类与类之间的关系。
+# 在Python中，可以在类定义语句中，类名右侧使用一对小括号将要继承的基类名称括起来，从而实现类的继承。
+# class ClassName(baseclasslist):
+#     # 类文档字符串
+#     """
+#     类的帮助信息
+#     """
+#     Statement  # 类体
+# ClassName：用于指定类名。
+# baseclasslist：用于指定要继承的基类，可以有多个，类名之间用逗号“,”分隔。如果不指定，将使用所有Python对象的根类object。
+# '''类的帮助信息'''：用于指定类的文档字符串，定义该字符串后，在创建类的对象时，输入类名和左侧的括号“(”后，将显示该信息。
+# statement：类体，主要由类变量（或类成员）、方法和属性等定义语句组成。如果在定义类时，没想好类的具体功能，也可以在类体中直接使用pass语句代替。
+
+class Fruit:
+    color = "green"
+
+    def harvest(self, color):
+        print("immaturity:" + Fruit.color)
+        print("after maturity:" + color)
+
+
+class Apple(Fruit):
+    color = "red"
+
+    def __init__(self):
+        print("I'm apple")
+
+
+class Orange(Fruit):
+    color = "orange"
+
+    def __init__(self):
+        print("I'm orange")
+
+
+apple = Apple()
+apple.harvest(apple.color)
+print()
+orange = Orange()
+orange.harvest(orange.color)
+print()
+
+
+# 虽然在Apple和Orange类中没有harvest()方法，但是Python允许派生类访问基类的方法。
+
+# 方法重写
+# 基类的成员都会被派生类继承，当基类中的某个方法不完全适用于派生类时，就需要在派生类中重写父类的这个方法。
+class Pear(Fruit):
+    color = "cyan"
+
+    def __init__(self):
+        print("I'm pear")
+
+    def harvest(self, color):
+        print("immature pear:" + Fruit.color)
+        print("mature pear:" + color)
+
+
+pear = Pear()
+pear.harvest(pear.color)
+print()
+
+
+# 派生类中调用基类的__init__()方法
+# 在派生类中定义__init__()方法时，不会自动调用基类的__init__()方法。
+# 要让派生类调用基类的__init__()方法进行必要的初始化，需要在派生类使用super()函数调用基类的__init__()方法。
+class Fruit:
+    def __init__(self, color="green"):
+        self.color = color
+        print("immaturity:" + self.color)
+
+    def harvest(self, colour):
+        print("after maturity:" + colour)
+
+
+class Apple(Fruit):
+    colour = "red"
+
+    def __init__(self):
+        print("I'm apple")
+        super().__init__()
+
+
+apple = Apple()
+apple.harvest(apple.colour)
+print()
